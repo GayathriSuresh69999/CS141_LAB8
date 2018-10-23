10a3ad1a0f6bd836f4a8297c11b32187144c08ff
GayathriSuresh69999 <42207654+GayathriSuresh69999@users.noreply.github.com>
2018-10-23 16:21:17 +0000

    Add files via upload

---
+++ lab8_q1.cpp
@@ -0,0 +1,37 @@
+// First include the libraries
+#include<iostream>
+using namespace std;
+
+//Function for finding the sum
+int sumarr(int a[],int n)
+{	
+int s=0;
+//varible for sum
+	
+//loop for finding the sum of array elements
+for (int i=0;i<n;i++)
+ s+=a[i];
+	
+//printing sum 
+cout<<" Sum of the elements of the array is "<<s;
+return 0;
+}
+//main function 
+int main()
+{	
+int a[10],n;
+ 	
+//ask user for the limit
+cout<<"\n Enter number of elements in the array ";
+ cin>>n;
+ 	
+//ask user for array elements 
+cout<<"\n Enter array elements ";
+ for(int i=0;i<n;i++)
+ cin>>a[i];
+ 	
+// call summing function 
+sumarr(a,n);
+return 0;
+}
+//end of program
